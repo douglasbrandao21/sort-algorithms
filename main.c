7a30bae14e8bbcd16a75e7f685cd6429707c5a28
douglasbrandao21 <douglasbrando5@gmail.com>
2021-02-16 17:21:55 -0300

    Recording time

--- main.c
+++ main.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
-#define INPUT_SIZE 20
+#define INPUT_SIZE 500
+
+float *vector;
+int size = 0;
+
+struct timespec initialSortTime, finalSortTime;
+
+void printTime(struct timespec initial, struct timespec final) {
+  if (final.tv_nsec < initial.tv_nsec) {
+        final.tv_nsec += 1000000000;
+        final.tv_sec--;
+  }
+
+  printf("%ld.%09ld\n", (long)(final.tv_sec - initial.tv_sec),
+        final.tv_nsec - initial.tv_nsec);
+}
 
 void generateInput() {
   FILE *input = fopen("./vetor.dat", "w");
 
+  srand((unsigned)time(NULL));
+
   for(int index = 0; index < INPUT_SIZE; index++) {
-    float randomNumber = ((float)rand()/RAND_MAX) * 100;
+    float randomNumber = ((float) rand()/RAND_MAX) * 100;
 
     fprintf(input, "%f\n", randomNumber);
   }
@@ -15,22 +34,50 @@ void generateInput() {
   fclose(input);
 }
 
-float *getInputVector() {
+void *getInputVector() {
   FILE *input = fopen("./vetor.dat", "r");
-  float *vector;
-  int index = 0;
 
-  vector = (float *) malloc(500000 * sizeof(float));
+  vector = (float *) malloc(INPUT_SIZE * sizeof(float));
 
-  while(fscanf(input, "%f", &vector[index]) != EOF) index++;
-  
-  return vector;
+  while(fscanf(input, "%f", &vector[size]) != EOF) size++;
+
+  fclose(input);
+}
+
+void insertionSort(){
+    clock_gettime(CLOCK_REALTIME, &initialSortTime);
+    
+    int i, j;
+    float key;
+
+    for (i = 1; i < size; i++) {
+        key = vector[i];
+        j = i - 1;
+
+        while (j >= 0 && vector[j] > key) {
+            vector[j + 1] = vector[j];
+            j = j - 1;
+        }
+        vector[j + 1] = key;
+    }
+
+    clock_gettime(CLOCK_REALTIME, &finalSortTime);
 }
 
 int main() {
-  generateInput();
+  clock_gettime(CLOCK_REALTIME, &initialSortTime);
 
-  float *vector = getInputVector();
+  // generateInput();
+
+  // getInputVector();
+
+  // insertionSort();
+
+  sleep(5);
+
+  clock_gettime(CLOCK_REALTIME, &finalSortTime);
+
+  printTime(initialSortTime, finalSortTime);
 
   return 0;
 }
